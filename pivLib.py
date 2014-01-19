4690f25f3ac0d2bd2df3c57e3d55987007eb7f33
brian <brn.j.williams@gmail.com>
2014-01-18 21:40:28 -0600

    Initialized content, added sample images

--- pivLib.py
+++ pivLib.py
@@ -1,1 +1,22 @@
-# PIV Lib
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.linalg
+from scipy import misc
+
+
+class pivEval(object):
+
+    def __init__(self, params):
+        #print Initializing
+        self.im1 = misc.imread(params['file1'])
+        self.im2 = misc.imread(params['file2'])
+
+        if params['show'] == 1:
+            plt.subplot(121)
+            plt.imshow(self.im1)
+            plt.subplot(122)
+            plt.imshow(self.im2)
+            plt.show()
+
+
+
---
+++ sCall.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pivLib
+
+
+file1 = 'synt0.png'
+file2 = 'synt1.png'
+show = 0
+
+params = {'file1':file1, 'file2':file2, 'show':show}
+
+c1 = pivLib.pivEval(params)
+
+
+
+
