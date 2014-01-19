4d1e3d50afceb8046ce98e27898074cc1593ea3e
brian <brn.j.williams@gmail.com>
2014-01-19 09:06:01 -0600

    added to cross-corr func

--- pivLib.py
+++ pivLib.py
@@ -1,3 +1,16 @@
+''' 
+pyPIV version 0.10
+
+Python implementation of Particle Image Velocimetry
+Brian Williams
+brn.j.williams@gmail.com
+2014.01.19
+
+'''
+
+############################################################
+############################################################
+
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -10,6 +23,8 @@ from scipy import misc
 import scipy.signal
 import scipy.ndimage
 
+############################################################
+############################################################
 
 class pivEval(object):
 
@@ -20,6 +35,10 @@ class pivEval(object):
         self.s0 = params['s0']
         self.nPasses = params['nPasses']
         self.sRatio = params['sRatio']
+        self.sigma = params['sigma']
+
+        self.x = np.zeros(np.shape(self.im1))
+        self.y = np.zeros(np.shape(self.im1))
 
         if params['show'] == 1:
             plt.subplot(121)
@@ -28,17 +47,52 @@ class pivEval(object):
             plt.imshow(self.im2)
             plt.show()
 
+        self.PIV_scan(50)
+        
 
+    #def generateGrid(self):
+
+
+    def PIV_scan(self, winSize):
+
+        im1 = blur(self.im1,self.sigma)
+        im2 = blur(self.im2,self.sigma)
+
+
+        im2_padded=np.zeros([im1.shape[0]+2*winSize,im1.shape[1]+2*winSize])
+        im2_padded[winSize:im2_padded.shape[0]-winSize,winSize:im2_padded.shape[1]-winSize] = im2
+        for i in range(0,1):#range(0,np.int(self.im1.shape[0]/winSize)):
+            for j in range(0,1):#range(0,np.int(self.im1.shape[1]/winSize)):
+                a = im1[i*winSize:(i+1)*winSize,j*winSize:(j+1)*winSize]
+                b = im2_padded[i*winSize:(i+3)*winSize,j*winSize:(j+3)*winSize]
+                c, maxIndex = xcorr(a,b)
+                self.x[i*winSize:(i+1)*winSize,j*winSize:(j+1)*winSize] = self.x[i+winSize:(i+1)*winSize,j*winSize:(j+1)*winSize] # + !!!!!!!!!!!!! Compute!!!
+
+        self.im2_padded = im2_padded
+        self.a = a
+        self.b = b
+        self.c = c
+        self.maxIndex = maxIndex
+
+
+
+############################################################
+############################################################
+############################################################
+############################################################
 
 def xcorr(a,b):
     c = scipy.signal.correlate2d(a.astype(float),b.astype(float))
-    return c
+    max_in = np.unravel_index(c.argmax(),c.shape)
+    return c, max_in
+    
+############################################################
 
-
-def blur(z, sigma=3):
+def blur(z, sigma=2):
     blurred = scipy.ndimage.gaussian_filter(z, sigma=sigma)
     return blurred
 
+############################################################
 
 def plotXCorr(Z):
     X = np.arange(0,Z.shape[0])
--- sCall.py
+++ sCall.py
@@ -6,13 +6,14 @@ import pivLib
 file1 = 'synt0.png'
 file2 = 'synt1.png'
 show = 0
+sigma = 2
 s0 = 50
 sRatio = 0.5
 nPasses = 3
 
-params = {'file1':file1, 'file2':file2, 'show':show, 's0':s0, 'sRatio':sRatio, 'nPasses':nPasses}
+params = {'file1':file1, 'file2':file2, 'show':show, 's0':s0, 'sRatio':sRatio, 'nPasses':nPasses, 'sigma':sigma}
 
-c1 = pivLib.pivEval(params)
+c = pivLib.pivEval(params)
 
 
 
