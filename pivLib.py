62706c7ad7132d4a8869a593880206d10c4b71cb
brian <brn.j.williams@gmail.com>
2014-01-18 22:49:07 -0600

    Added funcs, building main lib

--- pivLib.py
+++ pivLib.py
@@ -1,7 +1,14 @@
 import numpy as np
+
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
 import numpy.linalg
-from scipy import misc
+from scipy import misc 
+import scipy.signal
+import scipy.ndimage
 
 
 class pivEval(object):
@@ -10,6 +17,9 @@ class pivEval(object):
         #print Initializing
         self.im1 = misc.imread(params['file1'])
         self.im2 = misc.imread(params['file2'])
+        self.s0 = params['s0']
+        self.nPasses = params['nPasses']
+        self.sRatio = params['sRatio']
 
         if params['show'] == 1:
             plt.subplot(121)
@@ -20,3 +30,29 @@ class pivEval(object):
 
 
 
+def xcorr(a,b):
+    c = scipy.signal.correlate2d(a.astype(float),b.astype(float))
+    return c
+
+
+def blur(z, sigma=3):
+    blurred = scipy.ndimage.gaussian_filter(z, sigma=sigma)
+    return blurred
+
+
+def plotXCorr(Z):
+    X = np.arange(0,Z.shape[0])
+    Y = np.arange(0,Z.shape[1])
+    X, Y = np.meshgrid(X, Y)
+
+    fig = plt.figure()
+    #ax = fig.gca(projection='3d')
+    ax = fig.add_subplot(111, projection='3d')
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    #ax.set_zlim(-1.01,1.01)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
+    
--- sCall.py
+++ sCall.py
@@ -6,8 +6,11 @@ import pivLib
 file1 = 'synt0.png'
 file2 = 'synt1.png'
 show = 0
+s0 = 50
+sRatio = 0.5
+nPasses = 3
 
-params = {'file1':file1, 'file2':file2, 'show':show}
+params = {'file1':file1, 'file2':file2, 'show':show, 's0':s0, 'sRatio':sRatio, 'nPasses':nPasses}
 
 c1 = pivLib.pivEval(params)
 
